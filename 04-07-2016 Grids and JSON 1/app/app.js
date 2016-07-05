5d4d02b76bb5d9d9c700949a488d7a65f15870a6
shubhamsharma0003 <shubhamsharma0003@gmail.com>
2016-07-05 12:55:41 +0530

    a

--- 04-07-2016 Grids and JSON 1/app/app.js
+++ 04-07-2016 Grids and JSON 1/app/app.js
@@ -1,5 +1,10 @@
 var app = angular.module('app', []);
 
-app.controller('mainController', function($scope){
+app.controller('mainController', function($scope) {
+	$scope.repeat = [];
+	var timesToRepeat = 3;
+	for(var i=0; i<timesToRepeat; ++i) {
+		$scope.repeat.push(i);
+	}
 	
 });
