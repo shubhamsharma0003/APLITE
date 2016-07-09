0b5bd03f690130587470331e6ec20cbf716187d3
shubhamsharma0003 <shubhamsharma0003@gmail.com>
2016-07-09 12:28:08 +0530

    a

--- 08-07-2016--Responsive-form-/app/app.js
+++ 08-07-2016--Responsive-form-/app/app.js
@@ -1,10 +1,9 @@
-var app = angular.module("app", []);
-
-app.controller('formController', function($scope){
-	$scope.ages = [];
-	for(var a=18; a<=99; a++) {
-		$scope.ages.push(a);
-	}
+var app = angular.module("app", [])
+		 .controller('formController', function($scope){
+		  $scope.ages = [];
+		  for(var a=18; a<=99; a++) {
+		  	$scope.ages.push(a);
+		  }
 
 	// USER OBJECT
 	$scope.user = {
