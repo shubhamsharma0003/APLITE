43134de9874dbcb1b518e71050f9e262fa9e01ed
shubhamsharma0003 <shubhamsharma0003@gmail.com>
2016-07-11 12:17:50 +0530

    11-07-2016

--- 08-07-2016--Responsive-form-/app/app.js
+++ 08-07-2016--Responsive-form-/app/app.js
@@ -1,21 +1,51 @@
-var app = angular.module("app", [])
-		 .controller('formController', function($scope){
-		  $scope.ages = [];
-		  for(var a=18; a<=99; a++) {
-		  	$scope.ages.push(a);
-		  }
-
-	// USER OBJECT
-	$scope.user = {
-		name: "",
-		email: "",
-		password: "",
-		address: "",
-		clickFunction: function(x) {
-			alert("Button clicked with a value: " + x);
-		}
-	}; // USER OBJECT
+var app = angular.module("app", []);
 
 
-	
+	angular.module("app")
+		.value('userValue', {
+			name: "",
+			email: "",
+			password: "",
+			address: "",
+			counter: 0,
+			clickFunction: function(x) {
+				alert("Button clicked with a value: " + x);
+			},
+			blur: function(y) {
+				alert("Blur event caught on e-mail input with value: " + y);
+			},
+			change: function() {
+				this.counter++;
+			}
+		});
+
+
+	angular.module("app")
+		.controller('formController', function($scope, $log, userValue) {
+			$scope.ages = [];
+			for(var a=18; a<=99; a++) {
+				$scope.ages.push(a);
+			}
+
+			// $scope.counter = 0;
+
+			
+			$scope.user = userValue;
+
+			// USER OBJECT
+			// $scope.user = {
+			// 	name: "",
+			// 	email: "",
+			// 	password: "",
+			// 	address: "",
+			// 	clickFunction: function(x) {
+			// 		alert("Button clicked with a value: " + x);
+			// 	},
+			// 	blur: function(y) {
+			// 		alert("Blur event caught on e-mail input with value: " + y);
+			// 	},
+			// 	change: function() {
+			// 		$scope.counter++;
+			// 	}
+			// USER OBJECT
 });
