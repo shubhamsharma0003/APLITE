5c90a2808ef150fb759076e7b3cd30720282c207
shubhamsharma0003 <shubhamsharma0003@gmail.com>
2016-07-12 18:56:03 +0530

    a

--- 12-07-2016-Responsive-form-registration/app/app.js
+++ 12-07-2016-Responsive-form-registration/app/app.js
@@ -1,4 +1,7 @@
+// RESET THE FORM ON PAGE LOAD
 angular.module("app", ["ngMaterial"])
 		.controller('formController', ['$scope', function($scope){
 			$scope.myDate = new Date();
-		}]);
+		}]); // RESET THE FORM ON PAGE LOAD
+
+		
