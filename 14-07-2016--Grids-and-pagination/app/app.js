2638834e9ff0d6a37ab84020ae44cb86caa7e28c
shubhamsharma0003 <shubhamsharma0003@gmail.com>
2016-07-14 15:43:10 +0530

    a

--- 14-07-2016--Grids-and-pagination/app/app.js
+++ 14-07-2016--Grids-and-pagination/app/app.js
@@ -1,23 +1,24 @@
 var paginationApp = angular.module("paginationApp", [])
-					.controller('mainController', function($scope, $http){
+					.controller('mainController', function($scope, $http, $log){
 						$scope.gridsPerPage = 2;
 
 
 // +++++++++++++++++++++++++++++++++ CODE FOR GRIDS +++++++++++++++++++++++++++++++++++
 
-						$scope.repeat = [];
-							var timesToRepeat = 3;
-							for(var i=0; i<timesToRepeat; ++i) {
-								$scope.repeat.push(i);
-							}
+						// $scope.repeat = [];
+						// 	var timesToRepeat = 3;
+						// 	for(var i=0; i<timesToRepeat; ++i) {
+						// 		$scope.repeat.push(i);
+						// 	}
 							
-							$scope.content = [
-								{"heading" : "Heading 1"},
-								{"heading" : "Heading 2"},
-								{"heading" : "Heading 3"}
-							];
+						// 	$scope.content = [
+						// 		{"heading" : "Heading 1"},
+						// 		{"heading" : "Heading 2"},
+						// 		{"heading" : "Heading 3"}
+						// 	];
 
 // ++++++++++++++++++++++++++++++++ CODE FOR GRIDS ++++++++++++++++++++++++++++++++++++
+
 
 
 // +++++++++++++++++++++++++++++++++ CODE FOR GETTING XML DATA +++++++++++++++++++++++++++++++++++
@@ -26,8 +27,35 @@ var paginationApp = angular.module("paginationApp", [])
 	.then (function (response) {
 		$scope.data = response.data;
 		$scope.users = response.data.users;
+
+
+// ++++++++++++++++++++++++++++++++ GETTING JSON DATA IN ARRAY ++++++++++++++++++++++++++++++++++++
+
+	$scope.firstNames = [];
+	$scope.lastNames = [];
+	$scope.numbers = [];
+	$scope.emails = [];
+
+	for(var j = 0; j < 100; ++j) {
+		$scope.firstNames.push("$scope.users[j].Firstname");
+		$scope.lastNames.push("$scope.users[j].Lastname");
+		$scope.numbers.push("$scope.users[j].Mobile");
+		$scope.emails.push("$scope.users[j].Email");
+	}
+
+	$scope.q = $scope.users[0].Firstname;
+
+
+// ++++++++++++++++++++++++++++++++ GETTING JSON DATA IN ARRAY ++++++++++++++++++++++++++++++++++++
+
+		// alert($scope.users);
 	});
 
 // +++++++++++++++++++++++++++++++++ CODE FOR GETTING XML DATA +++++++++++++++++++++++++++++++++++
+
+
+
+
+
 
 					});	// END MAIN CONTROLLER
