29baa9518f66c86615d62e159f089a73c04dbce8
shubhamsharma0003 <shubhamsharma0003@gmail.com>
2016-07-22 16:52:05 +0530

    22-07-2016

---
+++ 12-07-2016-Responsive-form-registration - Copy/content/js/script.js
@@ -0,0 +1,40 @@
+function ClearForm(){
+    document.form.reset();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// FUNCTION FOR LOADING IMAGE INTO HTML AFTER IT GETS UPLOADED
+function readURL(input) {
+   if (input.files && input.files[0]) {
+       var reader = new FileReader();
+       
+       reader.onload = function (e) {
+           $('#thumbnail').attr('src', e.target.result);
+       };
+       
+       reader.readAsDataURL(input.files[0]);
+   	}
+}
+    
+$("#imgInp").change(function(){
+    readURL(this);
+});
+// END OF FUNCTION FOR LOADING IMAGE INTO HTML AFTER IT GETS UPLOADED
+
+// ---------------------------------------------------------------------------------------------------------------------
